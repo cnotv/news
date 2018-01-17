81ca5f77aed44bcdeda7063fba7ac6f86ddde893
cnotv <contact@cnotv.xyz>
2018-01-17 13:45:45 +0100

    gallery layout images have min height

--- src/components/postGallery.vue
+++ src/components/postGallery.vue
@@ -3,46 +3,53 @@
     class="o-gallery__item"
     v-if="this.post.data.preview"
   >
-    <a 
-      :href="this.post.data.url"
-      target="_blank"
+
+    <header>
+      <h6>{{ this.post.data.title | truncate(150) }}</h6>
+    </header>
+
+    <section
+      v-if="this.post.data.preview"
+      v-bind:style="style"
+      v-on:click="toggleModal(this.post.data.url)"
     >
-
-      <header>
-        <h6>{{ this.post.data.title | truncate(150) }}</h6>
-      </header>
-
-      <section
-        v-if="this.post.data.preview"
-        v-on:click="toggleModal(this.post.data.url)"
+      <a 
+        :href="this.post.data.url"
+        target="_blank"
       >
-        <img v-lazy="this.post.data.preview.images[0].variants.gif.source.url" v-if="this.post.data.preview.images[0].variants.gif" />
-        <img v-lazy="this.post.data.preview.images[0].source.url" v-else="this.post.data.preview" />
-      </section>
+        <img
+          v-if="this.post.data.preview.images[0].variants.gif"
+          v-lazy="this.post.data.preview.images[0].variants.gif.source.url"
+        />
+        <img
+          v-else="this.post.data.preview"
+          v-lazy="this.post.data.preview.images[0].source.url"
+        />
+      </a>
+    </section>
 
-      <footer>
-        <span v-if="this.post.data.ups">
-          <i class="fa fa-arrow-up"></i>
-          {{this.post.data.ups}}
-        </span>
-        <a 
-          :href="'http://www.reddit.com' + this.post.data.permalink"
-          target="_blank"
-        >
-          <i class="fa fa-comment"></i>
-        </a>
-        <small>
-          {{this.post.data.created_utc | date}}
-        </small>
-        <a
-          href="#"
-          class="c-btn-alt"
-          v-on:click="addSub(post.data.subreddit)"
-        >/r/{{this.post.data.subreddit}}
-        </a>
-      </footer>
+    <footer>
+      <span v-if="this.post.data.ups">
+        <i class="fa fa-arrow-up"></i>
+        {{this.post.data.ups}}
+      </span>
+      <a 
+        :href="'http://www.reddit.com' + this.post.data.permalink"
+        target="_blank"
+      >
+        <i class="fa fa-comment"></i>
+      </a>
+      <small>
+        {{this.post.data.created_utc | date}}
+      </small>
+      <a
+        href="#"
+        class="c-btn-alt"
+        v-on:click="addSub(post.data.subreddit)"
+      >/r/{{this.post.data.subreddit}}
+      </a>
+    </footer>
 
-    </a>
   </article>
 </template>
 
@@ -52,6 +59,9 @@ export default {
   props: ['post'],
   data () {
     return {
+      style: {
+        minHeight: this.post.data.preview.images[0].source.height / this.post.data.preview.images[0].source.width * 30 + 'vw'
+      }
     }
   }
 }
