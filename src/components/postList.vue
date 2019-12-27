6d5919153450b31e2508cf344d93832f0786113c
cnotv <contact@cnotv.xyz>
2019-12-27 22:42:50 +0100

    List: Add style; Style meta; Hide subreddit if not in search mode; Align comments right: Add comments counter

--- src/components/postList.vue
+++ src/components/postList.vue
@@ -6,22 +6,29 @@
       </a>
     </header>
 
-    <small>
-      <button v-on:click="addSub(this.post.data.subreddit)">
+    <small class="o-list__meta">
+      <button
+        v-if="getSearch.open"
+        v-on:click="addSub(this.post.data.subreddit)"
+      >
         <i class="fa fa-plus"></i>
         /r/{{ this.post.data.subreddit }}
       </button>
-      -
+      
       <a :href="'//' + this.post.data.domain" target="_blank">{{
         this.post.data.domain
       }}</a>
-      -
-      {{ this.post.data.created_utc | date }}
+
+      <span>{{ this.post.data.created_utc | date }}</span>
+
+      <span class="u-spacer--x"></span>
+
       <a
         :href="'http://www.reddit.com' + this.post.data.permalink"
         target="_blank"
       >
         <i class="fa fa-comment"></i>
+        {{this.post.data.num_comments}}
       </a>
     </small>
     <hr />
@@ -29,12 +36,19 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "List",
   props: ["post"],
   data() {
     return {};
-  }
+  },
+  computed: {
+    ...mapGetters([
+      "getSearch",
+    ])
+  },
 };
 </script>
 <style scoped>
