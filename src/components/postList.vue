0e9ca29cb73507457b2ab6d110e55f767efc3508
cnotv <contact@cnotv.xyz>
2019-12-28 21:07:50 +0100

    Post list: change meta order

--- src/components/postList.vue
+++ src/components/postList.vue
@@ -6,7 +6,13 @@
       </a>
     </header>
 
-    <small class="o-list__meta">
+    <small class="o-list__meta">      
+      <span>{{ this.post.data.created_utc | date }}</span>
+
+      <a :href="'//' + this.post.data.domain" target="_blank">{{
+        this.post.data.domain
+      }}</a>
+
       <button
         v-if="getSearch.open"
         v-on:click="addSub(this.post.data.subreddit)"
@@ -14,12 +20,6 @@
         <i class="fa fa-plus"></i>
         /r/{{ this.post.data.subreddit }}
       </button>
-      
-      <a :href="'//' + this.post.data.domain" target="_blank">{{
-        this.post.data.domain
-      }}</a>
-
-      <span>{{ this.post.data.created_utc | date }}</span>
 
       <span class="u-spacer--x"></span>
 
