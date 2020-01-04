5fb73cd071e6429dc5b0419457337fe923a745db
cnotv <contact@cnotv.xyz>
2020-01-04 10:58:46 +0100

    Router: Add redirection to home for missing path cases

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,16 +9,11 @@ const routes = [
     path: "/",
     name: "home",
     component: Home
+  },
+  {
+    path: "*",
+    redirect: "/"
   }
-  // {
-  //   path: "/about",
-  //   name: "about",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
-  // }
 ];
 
 const router = new VueRouter({
