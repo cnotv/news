080d83ee053e6944fe3f7902d5106c64d05ae5e1
cnotv <contact@cnotv.xyz>
2020-05-02 01:24:09 +0200

    Correct sorting issue

--- src/components/order.vue
+++ src/components/order.vue
@@ -1,6 +1,6 @@
 <template>
   <select class="c-input__select c-order" v-model="select">
-    <option v-for="sort in SORTING" v-bind:key="sort">{{ sort }}</option>
+    <option v-for="sort in sorting" v-bind:key="sort">{{ sort }}</option>
   </select>
 </template>
 
@@ -10,6 +10,12 @@ import { mapGetters, mapActions } from "vuex";
 import { SORTING } from "@/data/settings";
 
 export default Vue.extend({
+  data() {
+    return {
+      sorting: SORTING
+    };
+  },
+
   computed: {
     ...mapGetters(["getSort"]),
     select: {
