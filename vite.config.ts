dcbb019e3c184ede17e3ed17e11649629a0e7a28
Giuseppe Leo <giuseppe.leo@retresco.de>
2022-01-17 21:25:03 +0100

    Prevent CORS issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
-    }
+      "@": resolve(__dirname, "src"),
+    },
   },
   plugins: [vue()],
   define: {
-    'process.env': process.env
-  }
-})
+    "process.env": process.env,
+  },
+  build: {
+    base: "./",
+  },
+});
