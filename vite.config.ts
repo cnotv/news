68dbdcafbfc36a1501bf3752bc25db8bf0e2723f
cnotv <giuseppe.leo@suse.com>
2022-11-01 17:19:56 +0100

    Add PWA capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
@@ -10,7 +11,37 @@ export default defineConfig({
       '@': resolve(__dirname, 'src'),
     },
   },
-  plugins: [vue(), eslintPlugin()],
+  plugins: [
+    vue(),
+    eslintPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        sourcemap: true,
+      },
+      manifest: {
+        name: 'cnotv news',
+        short_name: 'cnotv-news',
+        description: 'cnotv news description',
+        theme_color: '#333',
+        icons: [
+          {
+            src: 'logo.svg',
+            sizes: '192x192',
+            type: 'image/svg',
+          },
+          {
+            src: 'logo.svg',
+            sizes: '512x512',
+            type: 'image/svg',
+          },
+        ],
+      },
+    }),
+  ],
   define: {
     'process.env': process.env,
   },
