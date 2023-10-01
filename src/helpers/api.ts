5cd9b4606f7cce231c101bc8be539f211c8e69de
cnotv <giuseppe.leo@suse.com>
2023-10-01 15:53:52 +0200

    Add retry and error catch

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,12 @@
 import { Posts, PostSub } from '@/types/api'
 import { RedditResponseChild, RedditResponseData } from '@/types/reddit-posts'
 import axios from 'axios'
+import axiosRetry from 'axios-retry'
 import { AxiosRequestConfig } from 'axios'
+
+axiosRetry(axios, {
+  retries: 3, // Number of retries (Defaults to 3)
+})
 
 function _subToPost(data: RedditResponseData): PostSub[] {
   return data.children.map((sub: RedditResponseChild) => {
@@ -22,12 +27,17 @@ export default {
     axios.defaults.baseURL = `//www.reddit.com/r/`
 
     // FIXME: Convert query into HTTP Parameters options
-    return axios.get(query).then((response) => {
-      return {
+    return axios
+      .get(query)
+      .then((response) => ({
         posts: response.data.data.children,
         after: response.data.data.after,
-      }
-    })
+      }))
+      .catch((error) => {
+        console.log('Error: ', error)
+
+        return { posts: [], after: '' }
+      })
   },
 
   async fetchSubs(q: string, after?: string): Promise<PostSub[]> {
@@ -41,6 +51,13 @@ export default {
     }
     const config: AxiosRequestConfig = { params }
 
-    return axios.get(url, config).then((response) => _subToPost(response.data.data))
+    return axios
+      .get(url, config)
+      .then((response) => _subToPost(response.data.data))
+      .catch((error) => {
+        console.log('Error: ', error)
+
+        return []
+      })
   },
 }
