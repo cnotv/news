d854ee127d3dc7e0d32f962b1477ddfa80d1869d
cnotv <giuseppe.leo@suse.com>
2024-02-15 20:49:59 +0100

    Enable video controls

--- src/components/ImagePreview.vue
+++ src/components/ImagePreview.vue
@@ -4,9 +4,9 @@
     :poster="data.preview?.images[0].source.url"
     preload="auto"
     autoplay="true"
-    muted="true"
     loop="true"
     webkit-playsinline=""
+    controls
     style="width: 100%; max-width: 100%; height: auto; max-height: 100vh"
   >
     <source :src="data.secure_media.reddit_video.fallback_url" type="video/mp4" />
@@ -17,9 +17,9 @@
     :poster="data.preview?.images[0].source.url"
     preload="auto"
     autoplay="true"
-    muted="true"
     loop="true"
     webkit-playsinline=""
+    controls
     style="width: 100%; max-width: 100%; height: auto; max-height: 100vh"
   >
     <source :src="data.preview?.reddit_video_preview?.fallback_url" type="video/mp4" />
